e7a27e49892473ca206917dbca7b5c11a5c9e060
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2017-09-19 21:42:17 +0200

    Ensure font awesome is properly added and works well with relative URLs

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,7 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-   .copy('node_modules/font-awesome/fonts', 'public/fonts');
+mix.options({ processCssUrls: false })
+	.js('resources/assets/js/app.js', 'public/js')
+	.sass('resources/assets/sass/app.scss', 'public/css')
+	.copy('node_modules/font-awesome/fonts', 'public/fonts');
