456bb4ee3f15d994c85d643bf5781a04d44ecd5c
Nicolas Perrenoud <mail@nicu.ch>
2017-10-01 15:03:35 +0200

    Check for enabled exec when fetching version

--- app/Util/ApplicationVersion.php
+++ app/Util/ApplicationVersion.php
@@ -8,13 +8,18 @@ class ApplicationVersion
 	const MINOR = 0;
 	const PATCH = 0;
 
+	private static function isEnabled($func) {
+		return is_callable($func) && false === stripos(ini_get('disable_functions'), $func);
+	}
+
 	public static function get()
 	{
-		$commitHash = trim(exec('git log --pretty="%h" -n1 HEAD'));
-
-		$commitDate = new \DateTime(trim(exec('git log -n1 --pretty=%ci HEAD')));
-		$commitDate->setTimezone(new \DateTimeZone('UTC'));
-
-		return sprintf('v%s.%s.%s-dev.%s (%s)', self::MAJOR, self::MINOR, self::PATCH, $commitHash, $commitDate->format('Y-m-d H:m:s'));
+		if (self::isEnabled('exec')) {
+			$commitHash = trim(exec('git log --pretty="%h" -n1 HEAD'));
+			$commitDate = new \DateTime(trim(exec('git log -n1 --pretty=%ci HEAD')));
+			$commitDate->setTimezone(new \DateTimeZone('UTC'));
+			return sprintf('v%s.%s.%s-dev.%s (%s)', self::MAJOR, self::MINOR, self::PATCH, $commitHash, $commitDate->format('Y-m-d H:m:s'));
+		}
+		return sprintf('v%s.%s.%s', self::MAJOR, self::MINOR, self::PATCH);
 	}
 }
