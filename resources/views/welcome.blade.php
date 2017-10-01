36948e088a008b96ff45939d6dd2163baa123d5b
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2017-10-02 01:53:22 +0200

    Styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,8 @@
 
 	<h1 class="display-4">Welcome</h1>
 	<p>Please select a module:</p>
-    <p>
-        <a href="{{ route('people.index') }}" class="btn btn-outline-primary btn-lg"><i class="fa fa-group"></i> <br>People</a> &nbsp;
-        <a href="{{ route('bank.index') }}" class="btn btn-outline-primary btn-lg"><i class="fa fa-bank"></i> <br>Bank</a> &nbsp;
-    </p>
-
+	<p>
+			<a href="{{ route('people.index') }}" class="btn btn-outline-primary btn-lg"><i class="fa fa-group"></i> <br>People</a> &nbsp;
+			<a href="{{ route('bank.index') }}" class="btn btn-outline-primary btn-lg"><i class="fa fa-bank"></i> <br>Bank</a>
+	</p>
 @endsection
