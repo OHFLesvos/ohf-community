38932b33c6a01e6d18fafb88336cf371479b8a6d
Nicolas Perrenoud <mail@nicu.ch>
2017-10-22 23:54:22 +0200

    Sluggable role

--- app/Role.php
+++ app/Role.php
@@ -2,15 +2,32 @@
 
 namespace App;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
+    use Sluggable;
+
     /**
      * The users that belong to the role.
      */
     public function users()
     {
         return $this->belongsToMany('App\User')->withTimestamps();
+    }
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
     }
 }
--- database/migrations/2017_10_22_202835_add_roles.php
+++ database/migrations/2017_10_22_202835_add_roles.php
@@ -16,6 +16,7 @@ class AddRoles extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('slug');
             $table->timestamps();
         });
     }
