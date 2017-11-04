463125ed7eb4a714d79537d856116d95a88a3b6a
Nicolas Perrenoud <mail@nicu.ch>
2017-11-04 14:29:35 +0100

    Added include file for head

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -1,29 +1,6 @@
 <!doctype html>
 <html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-		<!-- CSRF Token -->
-		<meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>@if(View::hasSection('title')) @yield('title') - @endif{{ Config::get('app.name') }} - {{ Config::get('app.product_name') }}</title>
-		
-        <link href="{{asset('css/app.css')}}?v={{ $app_version }}" rel="stylesheet" type="text/css">
-		
-		<link rel="icon" href="{{URL::asset('/img/favicon-32x32.png')}}" sizes="32x32" />
-		<link rel="icon" href="{{URL::asset('/img/favicon-192x192.png')}}" sizes="192x192" />
-		<link rel="apple-touch-icon-precomposed" href="{{URL::asset('/img/favicon-180x180.png')}}" />
-		
-		<!-- Scripts -->
-		<script>
-			window.Laravel = <?php echo json_encode([
-				'csrfToken' => csrf_token(),
-			]); ?>
-		</script>
-
-    </head>
+	@include('layouts.include.head')
     <body class="p-0 m-0 bg-light">
 
 		<div class="container">
