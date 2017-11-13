501534b82f901af4d929fd0879e0de87b0945322
Nicolas Perrenoud <mail@nicu.ch>
2017-11-13 23:45:38 +0100

    v1.5.0

--- config/app.php
+++ config/app.php
@@ -18,7 +18,7 @@ return [
 
 	'product_url' => 'https://github.com/mrcage/ohf-community',
 
-    'version' => 'v1.4.6',
+    'version' => 'v1.5.0',
 
     /*
     |--------------------------------------------------------------------------
