9b037c57f35bface03f97623ea871aba42268cba
Nicolas Perrenoud <mail@nicu.ch>
2017-11-19 15:03:17 +0100

    Dedicated js classes for people screen

--- resources/assets/js/people.js
+++ resources/assets/js/people.js
@@ -1,3 +1,5 @@
+pagination = require('./pagination.js');
+
 var delayTimer;
 
 $(function(){
@@ -41,8 +43,8 @@ function filterTable(page) {
             .attr('colspan', 10))
     );
 
-    var pagination = $('#paginator');
-    pagination.empty();
+    var paginator = $('#paginator');
+    paginator.empty();
 
     var paginationInfo = $( '#paginator-info' );
     paginationInfo.empty();
@@ -65,7 +67,7 @@ function filterTable(page) {
             $.each(result.data, function(k, v){
                 tbody.append(writeRow(v));
             });
-            updatePagination(pagination, result);
+            pagination.updatePagination(paginator, result, filterTable);
             paginationInfo.html( result.from + ' - ' + result.to + ' of ' + result.total );
         } else {
             tbody.append($('<tr>')
@@ -85,77 +87,6 @@ function filterTable(page) {
                     .attr('colspan', 10))
             );
         });
-}
-
-function updatePagination(container, result) {
-    // First page
-    if (result.current_page > 1) {
-        container.append( createPaginationItem( '&laquo;', 1, null ) );
-    } else {
-        container.append( createPaginationItem( '&laquo;', null, 'disabled' ) );
-    }
-
-    // Previous page
-    if (result.current_page > 1) {
-        container.append( createPaginationItem( '&lsaquo;', result.current_page - 1, null ) );
-    } else {
-        container.append( createPaginationItem( '&lsaquo;', null, 'disabled' ) );
-    }
-
-    // Pages before
-    for (i = 2 + Math.max(2 - ( result.last_page - result.current_page ), 0); i >= 1; i--) {
-        if (result.current_page > i) {
-            container.append( createPaginationItem( result.current_page - i, result.current_page - i, null ) );
-        }
-    }
-
-    // Current page
-    container.append( createPaginationItem( result.current_page, null, 'active' ) );
-
-    // Pages after
-    for (i = 1; i <= 2 + (Math.max(0, 3 - result.current_page)); i++) {
-        if ( result.current_page  + i - 1 < result.last_page ) {
-            container.append( createPaginationItem( result.current_page + i, result.current_page + i, null ) );
-        }
-    }
-
-    // Next page
-    if (result.current_page < result.last_page) {
-        container.append( createPaginationItem( '&rsaquo;', result.current_page + 1, null ) );
-    } else {
-        container.append( createPaginationItem( '&rsaquo;', null, 'disabled' ) );
-    }
-
-    // Last page
-    if (result.current_page < result.last_page) {
-        container.append( createPaginationItem( '&raquo;', result.last_page, null ) );
-    } else {
-        container.append( createPaginationItem( '&raquo;', null, 'disabled' ) );
-    }
-}
-
-function createPaginationItem( content, pageTarget, elemClass ) {
-    var elem = $( '<li>' )
-        .addClass( 'page-item' )
-    if ( pageTarget != null ) {
-        elem.append($( '<a>' )
-            .addClass( 'page-link' )
-            .attr('href', 'javascript:;')
-            .html( content )
-            .on('click', function(){
-                filterTable( pageTarget );
-            })
-        )
-    } else {
-        elem.append($( '<span>' )
-            .addClass( 'page-link' )
-            .html( content )
-        )
-    }
-    if ( elemClass != null ) {
-        elem.addClass( elemClass )
-    }
-    return elem;
 }
 
 function writeRow(person) {
