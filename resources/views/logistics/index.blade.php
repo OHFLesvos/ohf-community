81a15d8ee72bb476085223f14a52f422671a93f2
Your Name <mail@nicu.ch>
2017-12-26 12:51:50 +0100

    Mobile view for logistcs index

--- resources/views/logistics/index.blade.php
+++ resources/views/logistics/index.blade.php
@@ -3,7 +3,17 @@
 @section('title', 'Logistics')
 
 @section('content')
-<table class="table table-sm table-bordered table-striped table-hover">
+
+<div class="d-block d-sm-none">
+    @foreach (($projects)->filter(function($p){ return $p->has_article_mgmt; }) as $project)
+        <h3 class="display-4 mb-3">{{ $project->name }}</h3>
+        <p><a href="{{ route('logistics.articles.index', $project) }}" class="btn btn-secondary btn-block">
+            Manage articles
+        </a></p>
+    @endforeach
+</div>
+
+<table class="table table-sm table-bordered table-striped table-hover d-none d-sm-table">
     <thead>
         <tr>
             <th>Project</th>
