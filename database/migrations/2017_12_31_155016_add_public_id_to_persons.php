636a675f1d39f22818094571597a70d7078f37c2
Nicolas Perrenoud <mail@nicu.ch>
2017-12-31 18:52:52 +0200

    Added public id to person

---
+++ database/migrations/2017_12_31_155016_add_public_id_to_persons.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+use App\Person;
+
+class AddPublicIdToPersons extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('persons', function (Blueprint $table) {
+            $table->string('public_id', 32)->after('id');
+        });
+
+        foreach (Person::all() as $person) {
+            $person->public_id = Person::createUUID();
+            $person->save();
+        }
+
+        Schema::table('persons', function (Blueprint $table) {
+            $table->unique('public_id');
+        });
+}
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('persons', function (Blueprint $table) {
+            $table->dropColumn('public_id');
+        });
+    }
+}
