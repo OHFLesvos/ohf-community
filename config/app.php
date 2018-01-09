00c9f803e36b999a57050cf51226e4384a8b895c
Nicolas Perrenoud <mail@nicu.ch>
2018-01-09 15:20:22 +0200

    v1.7.3

--- config/app.php
+++ config/app.php
@@ -18,7 +18,7 @@ return [
 
 	'product_url' => 'https://github.com/mrcage/ohf-community',
 
-    'version' => 'v1.7.2',
+    'version' => 'v1.7.3',
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,13 @@
                     <a class="pull-right" href="{{ route('bank.index')  }}">Go to bank</a>
                 </div>
                 <div class="card-body">
-                    <p>Served <a href="{{ route('bank.index') }}?q=today:"><strong>{{ $num_people_served_today }}</strong></a> persons and handed out <strong>{{ $transaction_value_today }}</strong> drachma today.</p>
+                    <p>Served 
+                        @allowed('do-bank-withdrawals')
+                            <a href="{{ route('bank.withdrawal') }}?q=today:"><strong>{{ $num_people_served_today }}</strong></a> 
+                        @else
+                            <strong>{{ $num_people_served_today }}</strong>
+                        @endallowed
+                    persons and handed out <strong>{{ $transaction_value_today }}</strong> drachma today.</p>
                 </div>
             </div>
         @endallowed
