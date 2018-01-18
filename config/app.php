e1413ec4e3e056f40ad644c1ed567e42c8181917
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2018-01-18 09:39:02 +0100

    v1.8.11

--- config/app.php
+++ config/app.php
@@ -18,7 +18,7 @@ return [
 
 	'product_url' => 'https://github.com/mrcage/ohf-community',
 
-    'version' => 'v1.8.10',
+    'version' => 'v1.8.11',
 
     /*
     |--------------------------------------------------------------------------
