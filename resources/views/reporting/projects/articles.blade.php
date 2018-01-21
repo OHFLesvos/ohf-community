9f840beb2d140211d5d46a7250374ce0fd173312
Nicolas Perrenoud <mail@nicu.ch>
2018-01-21 13:33:58 +0100

    Improved formatting in articles report

--- resources/views/reporting/projects/articles.blade.php
+++ resources/views/reporting/projects/articles.blade.php
@@ -15,12 +15,11 @@
             @foreach($types as $type)
                 <div class="tab-pane fade" id="{{ $type }}" role="tabpanel" aria-labelledby="{{ $type }}-tab">
                     @if( ! $data[$type]->isEmpty() )
-                        <div class="table-responsive">
+                        <div class="table-responsive table-striped table-sm">
                             <table class="table">
                                 <thead>
                                     <tr>
                                         <th>Article</th>
-                                        <th>Unit</th>
                                         <th class="text-right">Today</th>
                                         <th class="text-right">Yesterday</th>
                                         <th class="text-right">This week</th>
@@ -35,17 +34,16 @@
                                     @foreach($data[$type] as $item)
                                         <tr>
                                             <td><a href="{{ route('reporting.article', $item['article']) }}">{{ $item['article']->name }}</a></td>
-                                            <td>{{ $item['article']->unit }}</td>
-                                            <td class="text-right">{{ $item['today'] }}</td>
-                                            <td class="text-right">{{ $item['yesterday'] }}</td>
-                                            <td class="text-right">{{ $item['this_week'] }}</td>
-                                            <td class="text-right">{{ $item['last_week'] }}</td>
-                                            <td class="text-right">{{ $item['this_month'] }}</td>
-                                            <td class="text-right">{{ $item['last_month'] }}</td>
-                                            <td class="text-right">{{ round($item['avg']) }}</td>
-                                            <td>
+                                            <td class="text-right">{{ $item['today'] }} @if(isset($item['today']))<small class="text-muted">{{ $item['article']->unit }}</small>@endif</td>
+                                            <td class="text-right">{{ $item['yesterday'] }} @if(isset($item['yesterday']))<small class="text-muted">{{ $item['article']->unit }}</small>@endif</td>
+                                            <td class="text-right">{{ $item['this_week'] }} @if(isset($item['this_week']))<small class="text-muted">{{ $item['article']->unit }}</small>@endif</td>
+                                            <td class="text-right">{{ $item['last_week'] }} @if(isset($item['last_week']))<small class="text-muted">{{ $item['article']->unit }}</small>@endif</td>
+                                            <td class="text-right">{{ $item['this_month'] }} @if(isset($item['this_month']))<small class="text-muted">{{ $item['article']->unit }}</small>@endif</td>
+                                            <td class="text-right">{{ $item['last_month'] }} @if(isset($item['last_month']))<small class="text-muted">{{ $item['article']->unit }}</small>@endif</td>
+                                            <td class="text-right">{{ round($item['avg']) }} @if(isset($item['avg']))<small class="text-muted">{{ $item['article']->unit }}</small>@endif</td>
+                                            <td class="text-right">
                                                 @if(isset($item['peak']))
-                                                    {{ $item['peak']->value }} <small class="text-muted">{{ $item['peak']->date }}</small>
+                                                    {{ $item['peak']->value }} <small class="text-muted">{{ $item['article']->unit }} ({{ $item['peak']->date }})</small>
                                                 @endif
                                             </td>
                                         </tr>
