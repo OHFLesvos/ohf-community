56eacacc3f5f22a26ab93c22601bf24609b8d15d
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2018-02-27 17:24:58 +0100

    Fix whitespace issue in dashboard widget

--- resources/views/dashboard/widgets/persons.blade.php
+++ resources/views/dashboard/widgets/persons.blade.php
@@ -5,7 +5,7 @@
     </div>
     <div class="card-body pb-2">
         <p>
-            @lang('people.there_are_n_people_registered', [ 'num_people' => $num_people ])@if($num_people_added_today > 0)(@lang('people.n_new_today', [ 'num_people_added_today' => $num_people_added_today ]))@endif.
+            @lang('people.there_are_n_people_registered', [ 'num_people' => $num_people ])@if($num_people_added_today > 0) (@lang('people.n_new_today', [ 'num_people_added_today' => $num_people_added_today ]))@endif.
         </p>
     </div>
 </div>
