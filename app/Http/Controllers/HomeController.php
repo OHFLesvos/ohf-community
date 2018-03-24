a97ee3adf00213ca7b57843eebbbfad5eff4c736
Nicolas Perrenoud <mail@nicu.ch>
2018-03-24 13:46:17 +0100

    Cleanup use

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Person;
-use App\Transaction;
-use App\User;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Gate;
 
 class HomeController extends Controller
 {
