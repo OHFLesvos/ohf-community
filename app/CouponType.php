88693df6ea85e29537b0e1c3cf090546d7d06d4d
Nicolas Perrenoud <mail@nicu.ch>
2018-03-26 00:02:57 +0200

    fix migration

--- app/CouponType.php
+++ app/CouponType.php
@@ -6,7 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class CouponType extends Model
 {
+    protected $fillable = [
+        'name',
+        'icon',
+        'daily_amount',
+        'retention_period',
+        'min_age',
+        'max_age',
+        'order',
+    ];
+
     public function couponHandouts() {
         return $this->hasMany('App\CouponHandout');
+    }
+
+    public function CouponReturns() {
+        return $this->hasMany('App\CouponReturn');
     }
 }
