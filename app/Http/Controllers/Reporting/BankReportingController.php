08dcca88adc13a88018bc36e206016ba2070cc92
Nicolas Perrenoud <mail@nicu.ch>
2018-03-25 22:27:00 +0200

    cleanup

--- app/Http/Controllers/Reporting/BankReportingController.php
+++ app/Http/Controllers/Reporting/BankReportingController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Reporting;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SelectDateRange;
-use App\Person;
 use App\CouponType;
 use App\CouponHandout;
 use App\CouponReturn;
