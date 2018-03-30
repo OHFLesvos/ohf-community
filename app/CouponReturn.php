9e4eb5d60316fd5e9abbf86e63d8d7bd90a9d89e
Nicolas Perrenoud <mail@nicu.ch>
2018-03-30 02:00:35 +0200

    Basic setup

--- app/CouponReturn.php
+++ app/CouponReturn.php
@@ -4,9 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class CouponReturn extends Model
+class CouponReturn extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
 
     public static function boot()
     {
