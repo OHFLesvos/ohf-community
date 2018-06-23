51eb38f9891c49b2593650fe837d90d169df5bf1
Nicolas Perrenoud <mail@nicu.ch>
2018-06-23 14:58:56 +0300

    accounting widget

--- resources/views/dashboard/widgets/accounting.blade.php
+++ resources/views/dashboard/widgets/accounting.blade.php
@@ -1,21 +1,22 @@
 <div class="card mb-4">
     <div class="card-header">
-        @lang('accounting.accounting') {{ $monthName }}
-        <span class="pull-right">
-            @can('create', App\MoneyTransaction::class)
-                <a href="{{ route('accounting.transactions.create')  }}">@lang('app.register')</a> &nbsp;
-            @endcan
-            <a href="{{ route('accounting.transactions.summary')  }}">@lang('app.view')</a>
-        </span>
+        <div class="row align-items-center">
+            <div class="col">@lang('accounting.accounting')</div>
+            <div class="col-auto">
+                @can('create', App\MoneyTransaction::class)
+                    <a href="{{ route('accounting.transactions.create')  }}" class="mr-1 btn btn-sm btn-outline-primary" title="@lang('app.register')">@icon(plus-circle)<span class="d-none d-xl-inline"> @lang('app.register')</span></a>
+                @endcan
+                <a href="{{ route('accounting.transactions.summary')  }}" class="btn btn-sm btn-outline-primary" title="@lang('app.view')">@icon(calculator)<span class="d-none d-xl-inline"> @lang('app.view')</span></a>
+            </div>
+        </div>
     </div>
-    <div class="card-body pb-2">
-        <p>
-            @isset($income)
-                @lang('accounting.income'): {{ number_format($income, 2) }}<br>
-            @endif
-            @isset($spending)
-                @lang('accounting.spending'): {{ number_format($spending, 2) }}
-            @endif
-        </p>
-    </div>
+    <table class="table mb-0">
+        <tr><th colspan="2">{{ $monthName }}:</td></tr>
+        @isset($income)
+            <tr><td>@lang('accounting.income')</td><td class="text-right">{{ number_format($income, 2) }}</td></tr>
+        @endif
+        @isset($spending)
+            <tr><td>@lang('accounting.spending')</td><td class="text-right">{{ number_format($spending, 2) }}</td></tr>
+        @endif
+    </table>
 </div>
