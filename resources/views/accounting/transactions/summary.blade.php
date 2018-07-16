0eb400adc96e6295c709882334763e1ebf3ad4d9
Nicolas Perrenoud <mail@nicu.ch>
2018-07-17 00:35:26 +0300

    simplified summary

--- resources/views/accounting/transactions/summary.blade.php
+++ resources/views/accounting/transactions/summary.blade.php
@@ -16,52 +16,31 @@
     </div>
     
     <div class="row">
-        <div class="col-sm">
-            <div class="card mb-4">
-                <div class="card-header">@lang('accounting.income')</div>
-                <table class="table table-strsiped mb-0">
-                    <tbody>
-                        @if(count($incomeByProject) > 0)
-                            @foreach($incomeByProject as $v)
-                                <tr>
-                                    <td>
-                                        <a href="{{ route('accounting.transactions.index') }}?filter[type]=income&filter[project]={{ $v->project }}&filter[date_start]={{ $monthDate->startOfMonth()->toDateString() }}&filter[date_end]={{ $monthDate->endOfMonth()->toDateString() }}">
-                                            {{ $v->project }}
-                                        </a>
-                                    </td>
-                                    <td class="text-right">{{ number_format($v->sum, 2) }}</td>
-                                </tr>
-                            @endforeach
-                        @else
-                            <tr><td><em>@lang('app.no_data_available_in_the_selected_time_range')</em></td></tr>
-                        @endif
-                    </tbody>
-                </table>
+        @foreach([ 'income' => $incomeByProject, 'spending' => $spendingByProject ] as $key => $projects)
+            <div class="col-sm">
+                <div class="card mb-4">
+                    <div class="card-header">@lang('accounting.' . $key)</div>
+                    <table class="table table-strsiped mb-0">
+                        <tbody>
+                            @if(count($projects) > 0)
+                                @foreach($projects as $v)
+                                    <tr>
+                                        <td>
+                                            <a href="{{ route('accounting.transactions.index') }}?filter[type]={{ $key }}&filter[project]={{ $v->project }}&filter[date_start]={{ $monthDate->startOfMonth()->toDateString() }}&filter[date_end]={{ $monthDate->endOfMonth()->toDateString() }}">
+                                                {{ $v->project }}
+                                            </a>
+                                        </td>
+                                        <td class="text-right">{{ number_format($v->sum, 2) }}</td>
+                                    </tr>
+                                @endforeach
+                            @else
+                                <tr><td><em>@lang('app.no_data_available_in_the_selected_time_range')</em></td></tr>
+                            @endif
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
-        <div class="col-sm">
-            <div class="card mb-4">
-                <div class="card-header">@lang('accounting.spending')</div>
-                <table class="table table-strsiped mb-0">
-                    <tbody>
-                        @if(count($spendingByProject) > 0)
-                            @foreach($spendingByProject as $v)
-                                <tr>
-                                    <td>
-                                        <a href="{{ route('accounting.transactions.index') }}?filter[type]=spending&filter[project]={{ $v->project }}&filter[date_start]={{ $monthDate->startOfMonth()->toDateString() }}&filter[date_end]={{ $monthDate->endOfMonth()->toDateString() }}">
-                                            {{ $v->project }}
-                                        </a>
-                                    </td>
-                                    <td class="text-right">{{ number_format($v->sum, 2) }}</td>
-                                </tr>
-                            @endforeach
-                        @else
-                            <tr><td><em>@lang('app.no_data_available_in_the_selected_time_range')</em></td></tr>
-                        @endif
-                    </tbody>
-                </table>
-            </div>
-        </div>
+        @endforeach
         <div class="col-sm">
             <div class="card mb-4">
                 <div class="card-header">@lang('app.total')</div>
