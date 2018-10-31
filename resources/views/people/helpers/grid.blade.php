959a69269abe03b097da8dbccb58902a5f3fbd4c
Nicolas Perrenoud <mail@nicu.ch>
2018-11-01 01:53:26 +0200

    nickname

--- resources/views/people/helpers/grid.blade.php
+++ resources/views/people/helpers/grid.blade.php
@@ -6,7 +6,11 @@
                     @if($item['model']->person->gender == 'f')@icon(female) 
                     @elseif($item['model']->person->gender == 'm')@icon(male) 
                     @endif
-                    {{ $item['model']->person->fullName }}
+                    @isset($item['model']->person->nickname)
+                        {{ $item['model']->person->nickname }}
+                    @else
+                        {{ $item['model']->person->name }}
+                    @endisset
                 </div>
                 <div class="card-body p-0">
                     @can('view', $item['model'])
@@ -23,6 +27,7 @@
                 </div>
                 <div class="card-body py-1 px-2">
                     <small>
+                        {{ $item['model']->person->name }} {{ strtoupper($item['model']->person->family_name) }}<br>
                         {{ $item['model']->person->age }}@if($item['model']->person->age != null && $item['model']->person->nationality != null),@endif
                         {{ $item['model']->person->nationality }}
                     </small>
