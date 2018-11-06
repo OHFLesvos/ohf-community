276f961c5571b7efe2a78c877227d110d2eaccea
Nicolas Perrenoud <mail@nicu.ch>
2018-11-06 19:55:11 +0200

    Fix message format

--- resources/views/bank/withdrawal.blade.php
+++ resources/views/bank/withdrawal.blade.php
@@ -10,10 +10,10 @@
 
         <div id="stats" class="text-center lead my-5">
             @if($numberOfPersonsServed > 0)
-                @lang('people.num_persons_served_handing_out_coupons', [
+                {!! __('people.num_persons_served_handing_out_coupons', [
                     'persons' => $numberOfPersonsServed,
                     'coupons' => $numberOfCouponsHandedOut,
-                ])
+                ]) !!}
             @else
                 @lang('people.not_yet_served_any_persons')
             @endif
