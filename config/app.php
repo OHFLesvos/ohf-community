1e89a2c86bbfc59e7aed06510368a3a951517d3f
Nicolas Perrenoud <mail@nicu.ch>
2018-11-25 16:14:02 +0200

    fix field name

--- config/app.php
+++ config/app.php
@@ -18,7 +18,8 @@ return [
 
 	'product_url' => 'https://github.com/mrcage/ohf-community',
 
-    'version' => 'v1.22.2',
+    'version' => 'v1.22.3',
+    
 
     /*
     |--------------------------------------------------------------------------
