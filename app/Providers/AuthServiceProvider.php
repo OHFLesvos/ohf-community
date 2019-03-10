cb4853320e64ffd21d2db2b8afc0716321ea7897
Nicolas Perrenoud <mail@nicu.ch>
2019-03-10 17:43:00 +0200

    Refactoring auth service provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,11 +13,11 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\User' => 'App\Policies\UserPolicy',
-        'App\Role' => 'App\Policies\RolePolicy',
-        'App\Person' => 'App\Policies\PersonPolicy',
-        'App\Task' => 'App\Policies\TaskPolicy',
-        'App\CalendarEvent' => 'App\Policies\CalendarEventPolicy',
+        \App\User::class => \App\Policies\UserPolicy::class,
+        \App\Role::class => \App\Policies\RolePolicy::class,
+        \App\Person::class => \App\Policies\PersonPolicy::class,
+        \App\Task::class => \App\Policies\TaskPolicy::class,
+        \App\CalendarEvent::class => \App\Policies\CalendarEventPolicy::class,
         \App\CalendarResource::class => \App\Policies\Calendar\ResourcePolicy::class,
         \App\Donor::class => \App\Policies\Fundraising\DonorPolicy::class,
         \App\Donation::class => \App\Policies\Fundraising\DonationPolicy::class,
@@ -30,6 +30,38 @@ class AuthServiceProvider extends ServiceProvider
         \App\LibraryLending::class => \App\Policies\Library\LibraryLendingPolicy::class,
     ];
 
+    protected $permission_gate_mappings = [
+        'manage-people' => 'people.manage',
+        'manage-helpers' => 'people.helpers.manage',
+        'create-badges' => 'badges.create',
+        'view-bank-index' => ['bank.withdrawals.do', 'bank.deposits.do', 'bank.configure'],
+        'do-bank-withdrawals' => 'bank.withdrawals.do',
+        'do-bank-deposits' => 'bank.deposits.do',
+        'validate-shop-coupons' => 'shop.coupons.validate',
+        'configure-shop' => 'shop.configure',
+        'view-barber-list' => 'shop.barber.list.view',
+        'configure-barber-list' => 'shop.barber.list.configure',
+        'operate-library' => 'library.operate',
+        'configure-library' => 'library.configure',
+        'view-bank-reports' => 'bank.statistics.view',
+        'view-people-reports' => 'people.reports.view',
+        'view-reports' => ['people.reports.view', 'bank.statistics.view', 'kitchen.reports.view', 'app.usermgmt.view'],
+        'view-usermgmt-reports' => 'app.usermgmt.view',
+        'configure-bank' => 'bank.configure',
+        'use-logistics' => 'logistics.use',
+        'accept-fundraising-webhooks' => 'fundraising.donations.accept_webhooks',
+        'view-accounting-summary' => 'accounting.summary.view',
+        'view-kitchen-reports' => 'kitchen.reports.view',
+        'view-calendar' => 'calendar.events.view',
+        'view-changelogs' => 'app.changelogs.view',
+        'view-logs' => 'app.logs.view',
+    ];
+   
+    protected $permission_gate_mappings_no_super_admin = [
+        'view-helpers-casework' => 'people.helpers.casework.view',
+        'manage-helpers-casework' => 'people.helpers.casework.manage',
+    ];
+
     /**
      * Register any authentication / authorization services.
      *
@@ -38,34 +70,12 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        $this->registerPermissionGateMappings();
+        $this->registerPErmissionGateMappingsNoSuperAdmin();
+    }
 
-        $simple_permission_gate_mappings = [
-            'manage-people' => 'people.manage',
-            'manage-helpers' => 'people.helpers.manage',
-            'create-badges' => 'badges.create',
-            'view-bank-index' => ['bank.withdrawals.do', 'bank.deposits.do', 'bank.configure'],
-            'do-bank-withdrawals' => 'bank.withdrawals.do',
-            'do-bank-deposits' => 'bank.deposits.do',
-            'validate-shop-coupons' => 'shop.coupons.validate',
-            'configure-shop' => 'shop.configure',
-            'view-barber-list' => 'shop.barber.list.view',
-            'configure-barber-list' => 'shop.barber.list.configure',
-            'operate-library' => 'library.operate',
-            'configure-library' => 'library.configure',
-            'view-bank-reports' => 'bank.statistics.view',
-            'view-people-reports' => 'people.reports.view',
-            'view-reports' => ['people.reports.view', 'bank.statistics.view', 'kitchen.reports.view', 'app.usermgmt.view'],
-            'view-usermgmt-reports' => 'app.usermgmt.view',
-            'configure-bank' => 'bank.configure',
-            'use-logistics' => 'logistics.use',
-            'accept-fundraising-webhooks' => 'fundraising.donations.accept_webhooks',
-            'view-accounting-summary' => 'accounting.summary.view',
-            'view-kitchen-reports' => 'kitchen.reports.view',
-            'view-calendar' => 'calendar.events.view',
-            'view-changelogs' => 'app.changelogs.view',
-            'view-logs' => 'app.logs.view',
-        ];
-        foreach ($simple_permission_gate_mappings as $gate => $permission) {
+    protected function registerPermissionGateMappings() {
+        foreach ($this->permission_gate_mappings as $gate => $permission) {
             Gate::define($gate, function ($user) use($permission) {
                 if ($user->isSuperAdmin()) {
                     return true;
@@ -80,12 +90,10 @@ class AuthServiceProvider extends ServiceProvider
                 return $user->hasPermission($permission);
             });
         }
+    }
 
-        $simple_permission_gate_mappings_no_super_admin = [
-            'view-helpers-casework' => 'people.helpers.casework.view',
-            'manage-helpers-casework' => 'people.helpers.casework.manage',
-        ];
-        foreach ($simple_permission_gate_mappings_no_super_admin as $gate => $permission) {
+    protected function registerPErmissionGateMappingsNoSuperAdmin() {
+        foreach ($this->permission_gate_mappings_no_super_admin as $gate => $permission) {
             Gate::define($gate, function ($user) use($permission) {
                 if (is_array($permission)) {
                     $hasPermission = false;
@@ -97,6 +105,5 @@ class AuthServiceProvider extends ServiceProvider
                 return $user->hasPermission($permission);
             });
         }
-
     }
 }
