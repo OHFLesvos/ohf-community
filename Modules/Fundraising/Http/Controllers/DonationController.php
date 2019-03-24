cbf83855cb62e091dc6ba3d40144768793a7362c
Nicolas Perrenoud <mail@nicu.ch>
2019-03-24 15:48:45 +0100

    Testing

--- Modules/Fundraising/Http/Controllers/DonationController.php
+++ Modules/Fundraising/Http/Controllers/DonationController.php
@@ -240,63 +240,84 @@ class DonationController extends Controller
      */
     public function raiseNowWebHookListener(Request $request) {
         $data = $request->all();
-
         Log::info("Donation webhook", $data);
 
-        if (!empty($request->stored_customer_firstname) && !empty($request->stored_customer_lastname) && !empty($request->stored_customer_street) && !empty($request->stored_customer_city)) {
-            $street = $request->stored_customer_street . (!empty($request->stored_customer_street_number) ? ' ' . $request->stored_customer_street_number : '');
-            $donor = Donor
-                ::where('first_name', $request->stored_customer_firstname)
-                ->where('last_name', $request->stored_customer_lastname)
-                ->where('street', $street)
-                ->where('city', $request->stored_customer_city)
-                ->first();
-            if ($donor == null) {
-                $donor = new Donor();
-                $donor->salutation = $request->stored_customer_salutation;
-                $donor->first_name = $request->stored_customer_firstname;
-                $donor->last_name = $request->stored_customer_lastname;
-                $donor->company = $request->stored_customer_company;
-                $donor->street = $street;
-                $donor->zip = $request->stored_customer_zip_code;
-                $donor->city = $request->stored_customer_city;
-                $donor->country_code = $request->stored_customer_country;
-                $donor->email = $request->stored_customer_email;
-                $donor->phone = $request->stored_customer_phone;
-                $donor->language = $request->language;
-                $donor->save();
-            }
+        $request->validate([
+            'stored_customer_firstname' => [
+                'required'
+            ],
+            'stored_customer_lastname' => [
+                'required'
+            ],
+            'stored_customer_street' => [
+                'required'
+            ],
+            'stored_customer_city' => [
+                'required'
+            ],
+            'amount' => [
+                'required'
+            ],
+            'currency' => [
+                'required'
+            ],
+            'payment_method' => [
+                'required'
+            ],
+        ]);
 
-            $date = new Carbon($request->created);
-            $amount = $request->amount / 100;
-            $currency = strtoupper($request->currency);
-            if ($currency != Config::get('fundraising.base_currency')) {
-                $exchange_rate = EzvExchangeRates::getExchangeRate($currency, $date);
-                $exchange_amount = $amount * $exchange_rate;
-            } else {
-                $exchange_amount = $amount;
-            }
-
-            $payment_methods = [
-                'ECA' => 'MasterCard',
-                'MPW' => 'MasterPass',
-                'PP'  => 'PayPal',
-                'VIS' => 'Visa',
-                'TWI' => 'Twint',
-                'PFC' => 'PostFinance Card',
-                'PEF' => 'PostFinance E-Finance',
-                'AMX' => 'American Express',
-            ];
-            $payment_method = $payment_methods[$request->payment_method] ?? $request->payment_method;
-            $donation = new Donation();
-            $donation->date = $date;
-            $donation->amount = $amount;
-            $donation->currency = $currency;
-            $donation->exchange_amount = $exchange_amount;
-            $donation->channel = 'RaiseNow (' . $payment_method . ')';
-            $donation->purpose = $request->stored_customer_message; // TODO consider stored_customer_purpose
-            $donation->reference = $request->epp_transaction_id;
-            $donor->donations()->save($donation);
+        $street = $request->stored_customer_street . (!empty($request->stored_customer_street_number) ? ' ' . $request->stored_customer_street_number : '');
+        $donor = Donor
+            ::where('first_name', $request->stored_customer_firstname)
+            ->where('last_name', $request->stored_customer_lastname)
+            ->where('street', $street)
+            ->where('city', $request->stored_customer_city)
+            ->first();
+        if ($donor == null) {
+            $donor = new Donor();
+            $donor->salutation = $request->stored_customer_salutation;
+            $donor->first_name = $request->stored_customer_firstname;
+            $donor->last_name = $request->stored_customer_lastname;
+            $donor->company = $request->stored_customer_company;
+            $donor->street = $street;
+            $donor->zip = $request->stored_customer_zip_code;
+            $donor->city = $request->stored_customer_city;
+            $donor->country_code = $request->stored_customer_country;
+            $donor->email = $request->stored_customer_email;
+            $donor->phone = $request->stored_customer_phone;
+            $donor->language = $request->language;
+            $donor->save();
         }
+
+        $date = new Carbon($request->created);
+        $amount = $request->amount / 100;
+        $currency = strtoupper($request->currency);
+        if ($currency != Config::get('fundraising.base_currency')) {
+            $exchange_rate = EzvExchangeRates::getExchangeRate($currency, $date);
+            $exchange_amount = $amount * $exchange_rate;
+        } else {
+            $exchange_amount = $amount;
+        }
+
+        $payment_methods = [
+            'ECA' => 'MasterCard',
+            'MPW' => 'MasterPass',
+            'PP'  => 'PayPal',
+            'VIS' => 'Visa',
+            'TWI' => 'Twint',
+            'PFC' => 'PostFinance Card',
+            'PEF' => 'PostFinance E-Finance',
+            'AMX' => 'American Express',
+        ];
+        $payment_method = $payment_methods[$request->payment_method] ?? $request->payment_method;
+        $donation = new Donation();
+        $donation->date = $date;
+        $donation->amount = $amount;
+        $donation->currency = $currency;
+        $donation->exchange_amount = $exchange_amount;
+        $donation->channel = 'RaiseNow (' . $payment_method . ')';
+        $donation->purpose = $request->stored_customer_message; // TODO consider stored_customer_purpose
+        $donation->reference = $request->epp_transaction_id;
+        $donor->donations()->save($donation);
     }
 }
