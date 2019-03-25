137126bc8efc73e1472a3eb76b1a8eb579c85628
Nicolas Perrenoud <mail@nicu.ch>
2019-03-25 10:34:46 +0100

    improved donor factory

--- Modules/Fundraising/Database/factories/ModelFactory.php
+++ Modules/Fundraising/Database/factories/ModelFactory.php
@@ -3,9 +3,16 @@
 use Modules\Fundraising\Entities\Donor;
 use Modules\Fundraising\Entities\Donation;
 
+use Illuminate\Support\Facades\Config;
+
+use MrCage\EzvExchangeRates\EzvExchangeRates;
+
 use Faker\Generator as Faker;
 
 $factory->define(Donor::class, function (Faker $faker) {
+    $countryCodeValidator = function($cc) {
+        return !in_array($cc, ['HM', 'BV']);
+    };
     $gender = $faker->randomElement(['male', 'female']);
     return [
         'salutation' => $faker->title($gender),
@@ -15,7 +22,7 @@ $factory->define(Donor::class, function (Faker $faker) {
         'street' => $faker->streetAddress,
         'zip' => $faker->postcode,
         'city' => $faker->city,
-        'country_code' => $faker->countryCode,
+        'country_code' => $faker->valid($countryCodeValidator)->countryCode,
         'email' => $faker->optional(0.8)->email,
         'phone' => $faker->optional(0.5)->phoneNumber,
         'language' => $faker->optional(0.3)->languageCode,
@@ -23,19 +30,24 @@ $factory->define(Donor::class, function (Faker $faker) {
 });
 
 $factory->define(Donation::class, function (Faker $faker) {
+    $baseCurrency = Config::get('fundraising.base_currency');
+    $currencyValidator = function($cc) use($baseCurrency) {
+        return in_array($cc, array_merge(Config::get('fundraising.currencies'), [$baseCurrency]));
+    };
     $date = $faker->dateTimeBetween('-5 years', 'now');
-    $currency = $faker->currencyCode;
     $amount = $faker->numberBetween(1, 10000);
+    $currency = $faker->valid($currencyValidator)->currencyCode;
+    $exchAmount = $currency != $baseCurrency ? EzvExchangeRates::getExchangeRate($currency) * $amount : $amount;
     return [
         'date' => $date,
         'amount' => $amount,
         'currency' => $currency,
-        'exchange_amount' => $amount, // TODO relate to currency
+        'exchange_amount' => $exchAmount,
         'channel' => $faker->randomElement(['Cash', 'Bank Transfer', 'PayPal', 'Stripe', 'RaiseNow']),
         'purpose' => $faker->optional(0.2)->sentence,
-        'reference' => $faker->optional(0.2)->uuid,
+        'reference' => $faker->optional(0.2)->bothify('********************'),
         'in_name_of' => $faker->optional(0.1)->name,
-        // 'thanked' => TODO
+        'thanked' => $faker->optional(0.1)->dateTimeBetween($date, 'now'),
     ];
 });
 
