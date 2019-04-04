2e73fe8a6d2ca44250e8c526019a29efa4fd81c8
Nicolas Perrenoud <mail@nicu.ch>
2019-04-04 19:26:11 +0200

    cleanup

--- app/Exports/BaseExport.php
+++ app/Exports/BaseExport.php
@@ -7,13 +7,7 @@ use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Events\BeforeExport;
-use Maatwebsite\Excel\Events\BeforeWriting;
 use Maatwebsite\Excel\Events\AfterSheet;
-
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
-use PhpOffice\PhpSpreadsheet\Style\Border;
-use PhpOffice\PhpSpreadsheet\Worksheet\PageSetup;
 
 abstract class BaseExport implements WithTitle, ShouldAutoSize, WithEvents
 {
