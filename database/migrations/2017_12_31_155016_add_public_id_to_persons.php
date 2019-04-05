12a61f194ae2d6cde20c890692d3aeefdf2d069e
Nicolas Perrenoud <mail@nicu.ch>
2019-04-05 21:35:03 +0200

    removed obsoilete person public_id record migration

--- database/migrations/2017_12_31_155016_add_public_id_to_persons.php
+++ database/migrations/2017_12_31_155016_add_public_id_to_persons.php
@@ -4,8 +4,6 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-use App\Person;
-
 class AddPublicIdToPersons extends Migration
 {
     /**
@@ -17,14 +15,6 @@ class AddPublicIdToPersons extends Migration
     {
         Schema::table('persons', function (Blueprint $table) {
             $table->string('public_id', 32)->after('id');
-        });
-
-        foreach (Person::withTrashed()->get() as $person) {
-            $person->public_id = Person::createUUID();
-            $person->save();
-        }
-
-        Schema::table('persons', function (Blueprint $table) {
             $table->unique('public_id');
         });
 }
