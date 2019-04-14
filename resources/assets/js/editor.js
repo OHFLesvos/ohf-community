5b2b21ac77a33896b452feb87fc02018d2a81b81
Nicolas Perrenoud <mail@nicu.ch>
2019-04-15 00:41:29 +0300

    Added laravel filemanager with summernote integration

--- resources/assets/js/editor.js
+++ resources/assets/js/editor.js
@@ -1,4 +1,33 @@
 $(document).ready(function() {
+
+    // Define function to open filemanager window
+    var lfm = function(options, cb) {
+        var route_prefix = (options && options.prefix) ? options.prefix : '/laravel-filemanager';
+        window.open(route_prefix + '?type=' + options.type || 'file', 'FileManager', 'width=900,height=600');
+        window.SetUrl = cb;
+    };
+
+    // Define LFM summernote button
+    var LFMButton = function(context) {
+        var ui = $.summernote.ui;
+        var button = ui.button({
+            contents: '<i class="note-icon-picture"></i> ',
+            tooltip: 'Insert image with filemanager',
+            click: function() {
+                lfm({type: 'image', prefix: '/laravel-filemanager'}, function(lfmItems, path) {
+                    if (Array.isArray(lfmItems)) {
+                        lfmItems.forEach(function (lfmItem) {
+                            context.invoke('insertImage', lfmItem.url);
+                        });
+                    } else {
+                        context.invoke('insertImage', lfmItems);
+                    }
+                });
+            }
+        });
+        return button.render();
+    };
+
     $('#editor').summernote({
         toolbar: [
             // [groupName, [list of button]]
@@ -6,9 +35,12 @@ $(document).ready(function() {
             ['style', ['bold', 'italic', 'underline', 'clear']],
             ['color', ['forecolor']],
             ['para', ['ul', 'ol']],
-            ['insert', ['link', 'picture', 'video', 'table']],
-            ['misc', ['undo', 'redo', 'fullscreen']]
+            ['insert', ['link', 'lfm', /*'picture', */ 'video', 'table']],
+            ['misc', ['undo', 'redo', 'fullscreen']],
         ],
+        buttons: {
+            lfm: LFMButton
+        },
         dialogsInBody: true
     });
 });
