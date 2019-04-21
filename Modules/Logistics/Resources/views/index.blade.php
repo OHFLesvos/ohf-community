c952b7f594e79425bbda4ca3ff6ceef1d3ac9757
Nicolas Perrenoud <mail@nicu.ch>
2019-04-21 21:33:35 +0300

    logistics index

--- Modules/Logistics/Resources/views/index.blade.php
+++ Modules/Logistics/Resources/views/index.blade.php
@@ -3,8 +3,17 @@
 @section('title', __('logistics::logistics.logistics'))
 
 @section('content')
-    <h1>Hello World</h1>
-    <p>
-        This view is loaded from module: {!! config('logistics.name') !!}
-    </p>
+    <div class="row">
+        @can('list', \Modules\Logistics\Entities\Supplier::class)
+            <div class="col">
+                <a href="{{ route('logistics.suppliers.index') }}" class="btn btn-primary btn-lg btn-block">@icon(location-arrow) @lang('logistics::suppliers.suppliers')</a>
+            </div>
+        @endcan
+        @can('list', \Modules\Logistics\Entities\Product::class)
+            <div class="col">
+                <a href="{{ route('logistics.products.index') }}" class="btn btn-primary btn-lg btn-block">@icon(shopping-basket) @lang('logistics::products.products')</a>
+            </div>
+        @endcan
+    </div>
+    
 @stop
