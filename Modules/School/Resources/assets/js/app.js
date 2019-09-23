67117e516292d2246d3a3d32e098effc3d4ed583
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2019-09-23 08:43:04 +0300

    vue reday for school

---
+++ Modules/School/Resources/assets/js/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+
+new Vue({
+    el: '#school-app'
+});
--- Modules/School/Resources/views/classes/students/index.blade.php
+++ Modules/School/Resources/views/classes/students/index.blade.php
@@ -4,42 +4,50 @@
 
 @section('content')
 
-    <h3>{{ $class->name }}</h3>
-    <p>
-        <strong>@lang('school::classes.teacher'):</strong> {{ $class->teacher_name }}<br>
-        <strong>@lang('app.period'):</strong> {{ $class->start_date->toDateString() }} - {{ $class->end_date->toDateString() }}<br>
-        <strong>@lang('school::classes.room'):</strong> {{ $class->room_name }}<br>
-        <strong>@lang('app.capacity'):</strong> {{ $class->students()->count() }} / {{ $class->capacity }}
-    </p>
-    @if( ! $class->students->isEmpty() )
-        <div class="table-responsive">
-            <table class="table table-sm table-bordered table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th class="">@lang('app.name')</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($class->students as $student)
-                        <tr>
-                            <td>
-                                @can('view', $student)
-                                    <a href="{{ route('people.show', $student) }}">
-                                @endcan
-                                @include('people::person-label', ['person'=> $student])
-                                @can('view', $student)
-                                    </a>
-                                @endcan
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    @else
-        @component('components.alert.info')
-            @lang('school::students.no_students_registered_in_class')
-        @endcomponent
-    @endif
+    <div id="school-app">
 
+        <h3>{{ $class->name }}</h3>
+        <p>
+            <strong>@lang('school::classes.teacher'):</strong> {{ $class->teacher_name }}<br>
+            <strong>@lang('app.period'):</strong> {{ $class->start_date->toDateString() }} - {{ $class->end_date->toDateString() }}<br>
+            <strong>@lang('school::classes.room'):</strong> {{ $class->room_name }}<br>
+            <strong>@lang('app.capacity'):</strong> {{ $class->students()->count() }} / {{ $class->capacity }}
+        </p>
+        @if( ! $class->students->isEmpty() )
+            <div class="table-responsive">
+                <table class="table table-sm table-bordered table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th class="">@lang('app.name')</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($class->students as $student)
+                            <tr>
+                                <td>
+                                    @can('view', $student)
+                                        <a href="{{ route('people.show', $student) }}">
+                                    @endcan
+                                    @include('people::person-label', ['person'=> $student])
+                                    @can('view', $student)
+                                        </a>
+                                    @endcan
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        @else
+            @component('components.alert.info')
+                @lang('school::students.no_students_registered_in_class')
+            @endcomponent
+        @endif
+
+    </div>
+
+@endsection
+
+@section('footer')
+    <script src="{{ asset('js/school.js') }}?v={{ $app_version }}"></script>
 @endsection
