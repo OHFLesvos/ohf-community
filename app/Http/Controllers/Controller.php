0246abedcfaf142f75df4ecb2b368489ee32711d
Nicolas Perrenoud <mail@nicu.ch>
2019-10-14 00:06:44 +0300

    Date range helpers added to controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,9 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+
+use Carbon\Carbon;
 
 class Controller extends BaseController
 {
@@ -19,4 +22,63 @@ class Controller extends BaseController
         // Ensure method index is automatically authorized using policy method 'list' if authorizeResource() is used
         return array_merge($this->traitResourceAbilityMap(), ['index' => 'list']);
     }
+
+    /**
+     * Parses optional date boundaries (year-month-day) from a request
+     */
+    protected static function getDatePeriodFromRequest(Request $request, $defaultDays = 30): array
+    {
+        // Validate request data
+        $request->validate([
+            'from' => [
+                'nullable',
+                'date',
+                'before_or_equal:to'
+            ],
+            'to' => [
+                'nullable',
+                'date',
+                'after_or_equal:from'
+            ],
+        ]);
+
+        // Return as array (from, to)
+        return [
+            $request->filled('from') ? new Carbon($request->input('from')) : Carbon::today()->subDays($defaultDays),
+            $request->filled('to') ? new Carbon($request->input('to')) : Carbon::today(),
+        ];
+    }
+
+    /**
+     * Parses optional date boundaries (year-month) from a request
+     */
+    protected static function getMonthRangeDatesFromRequest(Request $request): array
+    {
+        // Validate request data
+        $request->validate([
+            'year' => [
+                'numeric',
+                'required_with:month'
+            ],
+            'month' => [
+                'numeric',
+                'min:1',
+                'max:12',
+                'required_with:year'
+            ],
+        ]);
+
+        // Parse dates from request
+        $now = Carbon::now();
+        $year = $request->input('year', $now->year);
+        $month = $request->input('month', $now->month);
+        $from = Carbon::createFromDate($year, $month, 1)->startOfMonth();
+
+        // Return as array (from, to)
+        return [
+            $from,
+            $from->clone()->endOfMonth(),
+        ];
+    }
+
 }
--- app/Http/Controllers/Reporting/BaseReportingController.php
+++ app/Http/Controllers/Reporting/BaseReportingController.php
@@ -4,9 +4,6 @@ namespace App\Http\Controllers\Reporting;
 
 use App\Http\Controllers\Controller;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-
 use Carbon\Carbon;
 
 abstract class BaseReportingController extends Controller
@@ -57,63 +54,5 @@ abstract class BaseReportingController extends Controller
             $dates[$date->format('l')] = null;
         } while ($date->addDay()->lte(Carbon::now()->endOfWeek()));
         return collect($dates);
-    }
-
-    /**
-     * Parses optional date boundaries (year-month-day) from a request
-     */
-    protected static function getDatePeriodFromRequest(Request $request, $defaultDays = 30): array
-    {
-        // Validate request data
-        $request->validate([
-            'from' => [
-                'nullable',
-                'date',
-                'before_or_equal:to'
-            ],
-            'to' => [
-                'nullable',
-                'date',
-                'after_or_equal:from'
-            ],
-        ]);
-
-        // Return as array (from, to)
-        return [
-            $request->filled('from') ? new Carbon($request->input('from')) : Carbon::today()->subDays($defaultDays),
-            $request->filled('to') ? new Carbon($request->input('to')) : Carbon::today(),
-        ];
-    }
-
-    /**
-     * Parses optional date boundaries (year-month) from a request
-     */
-    protected static function getMonthRangeDatesFromRequest(Request $request): array
-    {
-        // Validate request data
-        $request->validate([
-            'year' => [
-                'numeric',
-                'required_with:month'
-            ],
-            'month' => [
-                'numeric',
-                'min:1',
-                'max:12',
-                'required_with:year'
-            ],
-        ]);
-
-        // Parse dates from request
-        $now = Carbon::now();
-        $year = $request->input('year', $now->year);
-        $month = $request->input('month', $now->month);
-        $from = Carbon::createFromDate($year, $month, 1)->startOfMonth();
-
-        // Return as array (from, to)
-        return [
-            $from,
-            $from->clone()->endOfMonth(),
-        ];
     }
 }
