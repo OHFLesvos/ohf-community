e5156e75d3be1aea9f29fbb506bbc232d1e7ea5a
Nicolas Perrenoud <mail@nicu.ch>
2019-11-11 16:07:43 +0100

    Helper seeder

--- Modules/Helpers/Database/Seeders/HelpersDatabaseSeeder.php
+++ Modules/Helpers/Database/Seeders/HelpersDatabaseSeeder.php
@@ -2,8 +2,13 @@
 
 namespace Modules\Helpers\Database\Seeders;
 
+use Modules\People\Entities\Person;
+use Modules\Helpers\Entities\Helper;
+
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+
+use Faker\Factory;
 
 class HelpersDatabaseSeeder extends Seeder
 {
@@ -16,6 +21,20 @@ class HelpersDatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call("OthersTableSeeder");
+        $faker = Factory::create();
+        $persons = factory(Person::class, 50)->create()->each(function($person) use($faker) {
+            $helper = factory(Helper::class)->make();
+            $person->helper()->save($helper);
+        });
+        
+        // factory(SchoolClass::class, 25)->create()->each(function($class) use($persons, $faker) {
+        //     $ids = $persons->random(mt_rand(0, $class->capacity))->pluck('id')->mapWithKeys(function($e) use ($faker) {
+        //         return [$e => [
+        //             'remarks' => $faker->optional(0.2)->sentence,
+        //         ]];
+        //     });
+        //     $class->students()->sync($ids);
+        // });
+
     }
 }
