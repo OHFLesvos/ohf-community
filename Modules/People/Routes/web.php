9b61b6227db9a01df731fa3253e66deeb25a6232
Nicolas Perrenoud <mail@nicu.ch>
2019-11-18 20:30:11 +0200

    Formatting

--- Modules/People/Routes/web.php
+++ Modules/People/Routes/web.php
@@ -21,19 +21,37 @@ Route::middleware(['auth', 'language'])
         Route::post('/people/bulkSearch', 'PeopleController@doBulkSearch')
             ->name('people.doBulkSearch')
             ->middleware('can:list,Modules\People\Entities\Person');
-        Route::get('/people/export', 'PeopleController@export')->name('people.export')->middleware('can:export,Modules\People\Entities\Person');
-        Route::get('/people/import', 'PeopleController@import')->name('people.import')->middleware('can:create,Modules\People\Entities\Person');
-        Route::post('/people/doImport', 'PeopleController@doImport')->name('people.doImport')->middleware('can:create,Modules\People\Entities\Person');
-        Route::get('/people/{person}/qrcode', 'PeopleController@qrCode')->name('people.qrCode')->middleware('can:view,person');
-        Route::get('/people/{person}/relations', 'PeopleController@relations')->name('people.relations');
-        Route::post('/people/{person}/relations', 'PeopleController@addRelation')->name('people.addRelation');
-        Route::delete('/people/{person}/children/{child}', 'PeopleController@removeChild')->name('people.removeChild');
-        Route::delete('/people/{person}/partner', 'PeopleController@removePartner')->name('people.removePartner');
-        Route::delete('/people/{person}/mother', 'PeopleController@removeMother')->name('people.removeMother');
-        Route::delete('/people/{person}/father', 'PeopleController@removeFather')->name('people.removeFather');
-        Route::get('/people/duplicates', 'PeopleController@duplicates')->name('people.duplicates');
-        Route::post('/people/duplicates', 'PeopleController@applyDuplicates')->name('people.applyDuplicates');
-        Route::post('/people/bulkAction', 'PeopleController@bulkAction')->name('people.bulkAction')->middleware('can:manage-people');
+        Route::get('/people/export', 'PeopleController@export')
+            ->name('people.export')
+            ->middleware('can:export,Modules\People\Entities\Person');
+        Route::get('/people/import', 'PeopleController@import')
+            ->name('people.import')
+            ->middleware('can:create,Modules\People\Entities\Person');
+        Route::post('/people/doImport', 'PeopleController@doImport')
+            ->name('people.doImport')
+            ->middleware('can:create,Modules\People\Entities\Person');
+        Route::get('/people/{person}/qrcode', 'PeopleController@qrCode')
+            ->name('people.qrCode')
+            ->middleware('can:view,person');
+        Route::get('/people/{person}/relations', 'PeopleController@relations')
+            ->name('people.relations');
+        Route::post('/people/{person}/relations', 'PeopleController@addRelation')
+            ->name('people.addRelation');
+        Route::delete('/people/{person}/children/{child}', 'PeopleController@removeChild')
+            ->name('people.removeChild');
+        Route::delete('/people/{person}/partner', 'PeopleController@removePartner')
+            ->name('people.removePartner');
+        Route::delete('/people/{person}/mother', 'PeopleController@removeMother')
+            ->name('people.removeMother');
+        Route::delete('/people/{person}/father', 'PeopleController@removeFather')
+            ->name('people.removeFather');
+        Route::get('/people/duplicates', 'PeopleController@duplicates')
+            ->name('people.duplicates');
+        Route::post('/people/duplicates', 'PeopleController@applyDuplicates')
+            ->name('people.applyDuplicates');
+        Route::post('/people/bulkAction', 'PeopleController@bulkAction')
+            ->name('people.bulkAction')
+            ->middleware('can:manage-people');
         Route::resource('/people', 'PeopleController');
 
         // Reporting
