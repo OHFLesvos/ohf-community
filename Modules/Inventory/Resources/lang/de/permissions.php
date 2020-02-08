d121bc8b1c34af4491945e9931795ae1fd25d669
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2020-02-08 20:38:34 +0200

    Removed unused module "Inventory"

--- Modules/Inventory/Resources/lang/de/permissions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'view_inventory_storage' => 'Inventar: Lager betrachten',
-    'manage_inventory_storage' => 'Inventar: Lager verwalten',
-    'create_inventory_transactions' => 'Inventar: Artikel lagern',
-    'delete_inventory_transactions' => 'Inventar: Artikel löschen',
-];
--- Modules/Inventory/Resources/lang/en/permissions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'view_inventory_storage' => 'Inventory: View storages',
-    'manage_inventory_storage' => 'Inventory: Manage storages',
-    'create_inventory_transactions' => 'Inventory: Store items',
-    'delete_inventory_transactions' => 'Inventory: Delete items',
-];
