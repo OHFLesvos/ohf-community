072ee43cb757eda748f17d346cddec481b83ffd2
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2020-02-09 18:38:25 +0200

    Removed unused "logistics" module

--- Modules/Logistics/Resources/lang/de/permissions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'view_suppliers' => 'Logistik: Lieferanten anzeigen',
-    'manage_suppliers' => 'Logistik: Lieferanten verwalten',
-    'view_products' => 'Logistik: Produkte anzeigen',
-    'manage_products' => 'Logistik: Produkte verwalten',
-];
--- Modules/Logistics/Resources/lang/en/permissions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'view_suppliers' => 'Logistics: View suppliers',
-    'manage_suppliers' => 'Logistics: Manage suppliers',
-    'view_products' => 'Logistics: View products',
-    'manage_products' => 'Logistics: Manage products',
-];
