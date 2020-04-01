5d8d39b3544078e2b2bea23cb4d9b64ce2c89f04
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2020-04-01 22:27:59 +0200

    Indexes for money transactions

--- database/migrations/2020_04_01_200559_add_money_transactions_indexes.php
+++ database/migrations/2020_04_01_200559_add_money_transactions_indexes.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class TextColumsForEncInMoneyTransactions extends Migration
+class AddMoneyTransactionsIndexes extends Migration
 {
     public function __construct()
     {
@@ -20,9 +20,13 @@ class TextColumsForEncInMoneyTransactions extends Migration
     public function up()
     {
         Schema::table('money_transactions', function (Blueprint $table) {
-            $table->text('beneficiary')->change();
-            $table->text('project')->change();
-            $table->text('description')->change();
+            $table->string('beneficiary')->change();
+            $table->string('project')->change();
+            $table->string('description')->change();
+
+            $table->index(['beneficiary']);
+            $table->index(['category']);
+            $table->index(['project']);
         });
     }
 
@@ -34,9 +38,13 @@ class TextColumsForEncInMoneyTransactions extends Migration
     public function down()
     {
         Schema::table('money_transactions', function (Blueprint $table) {
-            $table->string('beneficiary')->change();
-            $table->string('project')->change();
-            $table->string('description')->change();
+            $table->dropIndex(['beneficiary']);
+            $table->dropIndex(['category']);
+            $table->dropIndex(['project']);
+
+            $table->text('beneficiary')->change();
+            $table->text('project')->change();
+            $table->text('description')->change();
         });
     }
 }
