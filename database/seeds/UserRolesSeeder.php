b3f94921c59912ec6647c0f5c13daa90529d0436
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2020-04-05 18:34:21 +0200

    Added role admins seeder

--- database/seeds/UserRolesSeeder.php
+++ database/seeds/UserRolesSeeder.php
@@ -4,6 +4,7 @@ use App\Role;
 use App\RolePermission;
 use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
 
 class UserRolesSeeder extends Seeder
 {
@@ -19,6 +20,16 @@ class UserRolesSeeder extends Seeder
         });
         factory(User::class, 100)->create()->each(function ($user) use ($roles) {
             $user->roles()->attach($roles->random(mt_rand(0, 5))->unique()->values());
+        });
+
+        // Assign administrators to roles
+        $roles->each(function ($role) {
+            $num_users = $role->users->count();
+            if ($num_users > 0) {
+                $num_admins = mt_rand(0, min(3, $num_users));
+                $admins = Arr::random($role->users->pluck('id')->toArray(), $num_admins);
+                $role->administrators()->attach($admins);
+            }
         });
     }
 }
