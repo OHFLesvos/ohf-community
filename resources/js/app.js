ad22845f54402f3309a5173e1e0c33804e7dc82e
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2020-04-12 12:54:21 +0200

    Remove unused JS code

--- resources/js/app.js
+++ resources/js/app.js
@@ -74,42 +74,6 @@ $(function(){
 
 });
 
-/**
- * hashCode method for String
- */
-String.prototype.hashCode = function() {
-    var hash = 0, i, chr;
-    if (this.length === 0) return hash;
-    for (i = 0; i < this.length; i++) {
-      chr   = this.charCodeAt(i);
-      hash  = ((hash << 5) - hash) + chr;
-      hash |= 0; // Convert to 32bit integer
-    }
-    return hash;
-};
-
-/**
- * Remember open tab position of bootstrap 4 nav tabs
- * having the class "tab-remember". The container needs to have an id.
- */
-$(function(){
-    $('.tab-remember').each(function(){
-        var id = $(this).attr('id');
-        var key = id + "Clicked";
-        $(this).find('.nav-link').click(function(){
-            sessionStorage.setItem(key, $(this).attr('id').replace(/-tab$/, ''));
-        });
-        var activeTabId;
-        if (sessionStorage.getItem(key) != null) {
-            activeTabId = sessionStorage.getItem(key);
-        } else {
-            activeTabId = $(this).siblings('.tab-content').children('.tab-pane').attr('id');
-        }
-        $('#'+ activeTabId + '-tab').addClass('active');
-        $('#'+ activeTabId).addClass('active').addClass('show');
-    });
-});
-
 // Elements with the selector class gain focus and have their cursor set to the end
 $(function(){
     $('.focus-tail').each(function(){
