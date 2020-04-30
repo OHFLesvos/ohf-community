07040abc6ba34282dc0745502b6313ac6cc91f07
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2020-05-01 00:59:21 +0200

    don't use absolute routes in ziggy

--- resources/js/mixins/ziggyMixin.js
+++ resources/js/mixins/ziggyMixin.js
@@ -2,6 +2,7 @@ import route from 'ziggy'
 import { Ziggy } from '@/ziggy'
 export default {
     methods: {
-        route: (name, params, absolute) => route(name, params, absolute, Ziggy),
+        route: (name, params) => route(name, params, false, Ziggy),
+        // route: (name, params, absolute) => route(name, params, absolute, Ziggy),
     }
 }
