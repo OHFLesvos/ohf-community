6c877346adf055171bc3b57e00c29991180beca8
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2020-05-03 19:29:53 +0200

    Flexible aggregation column name

--- app/Mixins/BuilderMixin.php
+++ app/Mixins/BuilderMixin.php
@@ -9,22 +9,22 @@ class BuilderMixin
      */
     public function groupByDateGranularity()
     {
-        return function (?string $granularity = 'days', ?string $column = 'created_at', ?string $orderDirection = 'asc') {
+        return function (?string $granularity = 'days', ?string $column = 'created_at', ?string $orderDirection = 'asc', ?string $group_by_column_name = 'date_label') {
             switch ($granularity) {
                 case 'years':
-                    $this->query->selectRaw("YEAR($column) as date_label");
+                    $this->query->selectRaw("YEAR(`$column`) as `$group_by_column_name`");
                     break;
                 case 'months':
-                    $this->query->selectRaw("DATE_FORMAT($column, '%Y-%m') as date_label");
+                    $this->query->selectRaw("DATE_FORMAT(`$column`, '%Y-%m') as `$group_by_column_name`");
                         break;
                 case 'weeks':
-                    $this->query->selectRaw("DATE_FORMAT($column, '%x-W%v') as date_label");
+                    $this->query->selectRaw("DATE_FORMAT(`$column`, '%x-W%v') as `$group_by_column_name`");
                     break;
                 default: // days
-                    $this->query->selectRaw("DATE($column) as date_label");
+                    $this->query->selectRaw("DATE(`$column`) as `$group_by_column_name`");
             }
             return $this->query
-                ->groupBy('date_label')
+                ->groupBy($group_by_column_name)
                 ->orderBy($column, $orderDirection);
         };
     }
