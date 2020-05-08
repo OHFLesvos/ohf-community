b8e81664896a11ce5321c561ec5c8f53acd61f55
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2020-05-08 14:50:54 +0200

    Removed unused bsautocomplete

--- resources/views/components/form/bsAutocomplete.blade.php
+++
@@ -1,3 +0,0 @@
-@component('components.form.bsInput', [ 'name' => $name, 'label' => $label, 'help' => $help, 'attributes' => $attributes ])
-    @include('components.form.include.autocomplete')
-@endcomponent
--- resources/views/components/form/include/autocomplete.blade.php
+++
@@ -1,7 +0,0 @@
-{{ Form::text($name.'_search', null, array_merge([ 
-    'class' => 'form-control'.($errors->has($name) ? ' is-invalid' : ''),
-    'rel' => 'autocomplete',
-    'data-autocomplete-url' => $autocomplete_url,
-    'data-autocomplete-update' => '#'.form_id_string($name)
-], $attributes)) }}
-{{ Form::hidden($name, $value, [ 'id' => form_id_string($name) ]) }}
