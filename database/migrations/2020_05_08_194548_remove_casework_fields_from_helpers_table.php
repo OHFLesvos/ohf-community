7a111163c41290bf4d0a1f1a7b0ae60a30f51be3
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2020-05-08 23:05:17 +0200

    Fix migration

--- database/migrations/2020_05_08_194548_remove_casework_fields_from_helpers_table.php
+++ database/migrations/2020_05_08_194548_remove_casework_fields_from_helpers_table.php
@@ -21,47 +21,47 @@ class RemoveCaseworkFieldsFromHelpersTable extends Migration
                 $notes .= "Background: $helper->work_background\n";
             }
             if (filled($helper->has_tax_number)) {
-                $value = $helper->has_tax_number ? 'Yes' : 'No';
-                $notes .= "Tax number: $value\n";
+                $tax_number_states = collect([
+                    'yes' => 'Yes',
+                    'no' => 'No',
+                    'applied' => 'Applied',
+                ]);
+                $notes .= "Tax number: " .  $tax_number_states->get($helper->has_tax_number, $helper->has_tax_number) . "\n";
             }
             if (filled($helper->endorses_casework)) {
-                if ($helper->endorses_casework === false) {
-                    $notes .= "Case work: No\n";
-                } else {
-                    $notes .= "Case work: Yes\n";
-                    if (filled($helper->casework_case_number)) {
-                        $notes .= "Case number: $helper->casework_case_number\n";
-                    }
-                    if (filled($helper->casework_asylum_request_status)) {
-                        $asylum_request_states = collect([
-                            'awaiting_interview' => 'Awaiting interview',
-                            'waiting_for_decision' => 'Waiting for decision',
-                            'first_rejection' => 'First rejection',
-                            'second_rejection' => 'Second rejection',
-                            'subsidiary_protection' => 'Subsidiary protection',
-                            'refugee_status' => 'Refugee status',
-                        ]);
-                        $notes .= "Asylum Request Status: " . $asylum_request_states->get($helper->casework_asylum_request_status, $helper->casework_asylum_request_status) . "\n";
-                    }
-                    if (filled($helper->casework_has_geo_restriction)) {
-                        $value = $helper->casework_has_geo_restriction ? 'Yes' : 'No';
-                        $notes .= "Has geographical Restriction? $value\n";
-                    }
-                    if (filled($helper->casework_has_id_card)) {
-                        $value = $helper->casework_has_id_card ? 'Yes' : 'No';
-                        $notes .= "Has ID card? $value\n";
-                    }
-                    if (filled($helper->casework_has_passport)) {
-                        $value = $helper->casework_has_passport ? 'Yes' : 'No';
-                        $notes .= "Has passport? $value\n";
-                    }
-                    if (filled($helper->casework_vulnerability)) {
-                        $value = $helper->casework_vulnerability ? 'Yes' : 'No';
-                        $notes .= "Vulnerability: $value\n";
-                    }
-                }
+                $value = $helper->has_tax_number ? 'Yes' : 'No';
+                $notes .= "Case work: $value\n";
             }
-
+            if (filled($helper->casework_case_number)) {
+                $notes .= "Case number: $helper->casework_case_number\n";
+            }
+            if (filled($helper->casework_asylum_request_status)) {
+                $asylum_request_states = collect([
+                    'awaiting_interview' => 'Awaiting interview',
+                    'waiting_for_decision' => 'Waiting for decision',
+                    'first_rejection' => 'First rejection',
+                    'second_rejection' => 'Second rejection',
+                    'subsidiary_protection' => 'Subsidiary protection',
+                    'refugee_status' => 'Refugee status',
+                ]);
+                $notes .= "Asylum Request Status: " . $asylum_request_states->get($helper->casework_asylum_request_status, $helper->casework_asylum_request_status) . "\n";
+            }
+            if (filled($helper->casework_has_geo_restriction)) {
+                $value = $helper->casework_has_geo_restriction ? 'Yes' : 'No';
+                $notes .= "Has geographical Restriction? $value\n";
+            }
+            if (filled($helper->casework_has_id_card)) {
+                $value = $helper->casework_has_id_card ? 'Yes' : 'No';
+                $notes .= "Has ID card? $value\n";
+            }
+            if (filled($helper->casework_has_passport)) {
+                $value = $helper->casework_has_passport ? 'Yes' : 'No';
+                $notes .= "Has passport? $value\n";
+            }
+            if (filled($helper->casework_vulnerability)) {
+                $value = $helper->casework_vulnerability ? 'Yes' : 'No';
+                $notes .= "Vulnerability: $value\n";
+            }
             if (filled(trim($notes))) {
                 $helper->notes = trim($helper->notes . "\n\n" . $notes);
                 $helper->save();
