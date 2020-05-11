05f8b2d72226690951a1d24f5ac3cabf5eb3b1d7
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2020-05-11 11:51:51 +0200

    Added base JS api

---
+++ resources/js/api/baseApi.js
@@ -0,0 +1,39 @@
+import axios from '@/plugins/axios'
+import ziggyMixin from '@/mixins/ziggyMixin'
+export const route = ziggyMixin.methods.route
+
+import { getAjaxErrorMessage } from '@/utils'
+export const api = {
+    async get (url) {
+        try {
+            let res = await axios.get(url)
+            return res.data
+        } catch (err) {
+            throw getAjaxErrorMessage(err)
+        }
+    },
+    async post (url, data) {
+        try {
+            const res = await axios.post(url, data)
+            return res.data
+        } catch (err) {
+            throw getAjaxErrorMessage(err)
+        }
+    },
+    async put (url, data) {
+        try {
+            const res = await axios.put(url, data)
+            return res.data
+        } catch (err) {
+            throw getAjaxErrorMessage(err)
+        }
+    },
+    async delete (url) {
+        try {
+            const res = await axios.delete(url)
+            return res.data
+        } catch (err) {
+            throw getAjaxErrorMessage(err)
+        }
+    }
+}
