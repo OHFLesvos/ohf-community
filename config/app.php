6f7b383e8cee8c16e84505b3bcc683b7b5d36105
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2020-05-12 14:54:05 +0200

    2.19.0

--- config/app.php
+++ config/app.php
@@ -19,7 +19,7 @@ return [
 
     'product_url' => 'https://github.com/OHFLesvos/ohf-community',
 
-    'version' => 'v2.18.0',
+    'version' => 'v2.19.0',
     /*
     |--------------------------------------------------------------------------
     | Application Environment
