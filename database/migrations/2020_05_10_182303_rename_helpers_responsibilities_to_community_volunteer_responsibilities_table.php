fac12470f2423351fef0ff080ecac5de32e60371
Nicolas Perrenoud <mail@nicu.ch>
2020-07-19 16:47:32 +0300

    Fix issue with migration and mysql foreign key

--- database/migrations/2020_05_10_182303_rename_helpers_responsibilities_to_community_volunteer_responsibilities_table.php
+++ database/migrations/2020_05_10_182303_rename_helpers_responsibilities_to_community_volunteer_responsibilities_table.php
@@ -14,9 +14,19 @@ class RenameHelpersResponsibilitiesToCommunityVolunteerResponsibilitiesTable ext
     public function up()
     {
         Schema::rename('helpers_responsibilities', 'community_volunteer_responsibilities');
+        Schema::table('helpers_helper_responsibility', function (Blueprint $table) {
+            $table->dropForeign(['helper_id']);
+        });
         Schema::rename('helpers_helper_responsibility', 'community_volunteer_responsibility');
         Schema::table('community_volunteer_responsibility', function (Blueprint $table) {
             $table->renameColumn('helper_id', 'community_volunteer_id');
+        });
+        Schema::table('community_volunteer_responsibility', function (Blueprint $table) {
+            $table->foreign('community_volunteer_id', 'community_volunteer_responsibility_cv_id_foreign')
+                ->references('id')
+                ->on('community_volunteers')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
         });
     }
 
@@ -28,9 +38,19 @@ class RenameHelpersResponsibilitiesToCommunityVolunteerResponsibilitiesTable ext
     public function down()
     {
         Schema::table('community_volunteer_responsibility', function (Blueprint $table) {
+            $table->dropForeign(['cv_id']);
+        });
+        Schema::table('community_volunteer_responsibility', function (Blueprint $table) {
             $table->renameColumn('community_volunteer_id', 'helper_id');
         });
         Schema::rename('community_volunteer_responsibility', 'helpers_helper_responsibility');
+        Schema::table('helpers_helper_responsibility', function (Blueprint $table) {
+            $table->foreign('helper_id')
+                ->references('id')
+                ->on('community_volunteers')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
         Schema::rename('community_volunteer_responsibilities', 'helpers_responsibilities');
     }
 }
