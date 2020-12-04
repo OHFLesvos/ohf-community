c3963dbfc11842221855617aadca14befcd15a50
Nicolas Perrenoud <mail@nicu.ch>
2020-12-04 12:11:36 +0200

    v2.28.1

--- config/app.php
+++ config/app.php
@@ -19,7 +19,7 @@ return [
 
     'product_url' => 'https://github.com/OHFLesvos/ohf-community',
 
-    'version' => 'v2.28.0',
+    'version' => 'v2.28.1',
     /*
     |--------------------------------------------------------------------------
     | Application Environment
