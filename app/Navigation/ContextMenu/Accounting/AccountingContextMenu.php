bd474156138621987a7d260e48f807407bb69b91
Nicolas Perrenoud <mail@nicu.ch>
2020-12-06 22:11:31 +0200

    Add suppliers menu item

--- app/Navigation/ContextMenu/Accounting/AccountingContextMenu.php
+++ app/Navigation/ContextMenu/Accounting/AccountingContextMenu.php
@@ -4,6 +4,7 @@ namespace App\Navigation\ContextMenu\Accounting;
 
 use App\Navigation\ContextMenu\ContextMenu;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\View\View;
 
 class AccountingContextMenu implements ContextMenu
@@ -12,6 +13,12 @@ class AccountingContextMenu implements ContextMenu
     {
         $wallet = $view->getData()['wallet'];
         return [
+            'suppliers' => [
+                'url' => route('accounting.suppliers'),
+                'caption' => __('accounting.suppliers'),
+                'icon' => 'truck',
+                'authorized' => Auth::user()->can('viewAny', Supplier::class) || Gate::allows('manage-suppliers'),
+            ],
             'book' => [
                 'url' => route('accounting.webling.index', $wallet),
                 'caption' => __('accounting.book_to_webling'),
