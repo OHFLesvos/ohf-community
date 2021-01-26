4f292fb3cf85b73b6cbd1ab41c92e6afa98fe3eb
Nicolas Perrenoud <mail@nicu.ch>
2021-01-27 00:36:13 +0200

    Format of library export page

--- resources/js/pages/library/ExportPage.vue
+++ resources/js/pages/library/ExportPage.vue
@@ -1,52 +1,51 @@
 <template>
-    <b-form
-        v-if="loaded"
-        @submit.stop.prevent="onSubmit"
-    >
-
-        <!-- File format -->
-        <b-form-group
-            :label="$t('app.file_format')"
-        >
-            <b-form-radio-group
-                v-model="form.format"
-                :options="formats"
-                stacked
-            />
-        </b-form-group>
-
-        <!-- Selection -->
-        <b-form-group
-            :label="$t('app.selection')"
-        >
-            <b-form-radio-group
-                v-model="form.selection"
-                :options="selections"
-                stacked
-            />
-        </b-form-group>
-
-        <p>
-            <b-button
-                type="submit"
-                variant="primary"
-                :disabled="isBusy"
+    <b-container>
+        <b-form v-if="loaded" @submit.stop.prevent="onSubmit">
+            <b-card
+                :header="$t('app.export')"
+                class="shadow-sm mb-4"
+                footer-class="text-right"
             >
-                <font-awesome-icon icon="download" />
-                {{ $t('app.export') }}
-            </b-button>
+                <!-- File format -->
+                <b-form-group :label="$t('app.file_format')">
+                    <b-form-radio-group
+                        v-model="form.format"
+                        :options="formats"
+                        stacked
+                    />
+                </b-form-group>
+
+                <!-- Selection -->
+                <b-form-group :label="$t('app.selection')">
+                    <b-form-radio-group
+                        v-model="form.selection"
+                        :options="selections"
+                        stacked
+                    />
+                </b-form-group>
+                <template #footer>
+                    <b-button
+                        type="submit"
+                        variant="primary"
+                        :disabled="isBusy"
+                    >
+                        <font-awesome-icon :icon="isBusy ? 'spinner' : 'download'" :spin="isBusy" />
+                        {{ $t("app.export") }}
+                    </b-button>
+                </template>
+            </b-card>
+        </b-form>
+        <p v-else>
+            {{ $t("app.loading") }}
         </p>
-    </b-form>
-    <p v-else>
-        {{ $t('app.loading') }}
-    </p>
+    </b-container>
 </template>
 
 <script>
-import libraryApi from '@/api/library'
-import { postRequest } from '@/utils/form'
+import libraryApi from "@/api/library";
+import { postRequest } from "@/utils/form";
 export default {
-    data () {
+    data() {
         return {
             loaded: false,
             form: {
@@ -55,37 +54,37 @@ export default {
             },
             isBusy: false,
             formats: null,
-            selections: null,
-        }
+            selections: null
+        };
     },
-    async created () {
+    async created() {
         try {
-            let data = await libraryApi.fetchExportData()
-            this.form.format = data.format
-            this.form.selection = data.selection
+            let data = await libraryApi.fetchExportData();
+            this.form.format = data.format;
+            this.form.selection = data.selection;
             this.formats = Object.entries(data.formats).map(e => ({
                 value: e[0],
                 text: e[1]
-            }))
+            }));
             this.selections = Object.entries(data.selections).map(e => ({
                 value: e[0],
                 text: e[1]
-            }))
-            this.loaded = true
+            }));
+            this.loaded = true;
         } catch (err) {
-            alert(err)
+            alert(err);
         }
     },
     methods: {
-        async onSubmit () {
-            this.isBusy = true
+        async onSubmit() {
+            this.isBusy = true;
             try {
-                postRequest(this.route('api.library.doExport'), this.form)
+                postRequest(this.route("api.library.doExport"), this.form);
             } catch (err) {
-                alert(err)
+                alert(err);
             }
-            this.isBusy = false
+            this.isBusy = false;
         }
     }
-}
+};
 </script>
