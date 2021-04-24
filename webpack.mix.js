b113e95f4bdf90aed181498d935b52746936d301
Nicolas Perrenoud <mail@nicu.ch>
2021-04-24 19:55:21 +0300

    updated npm

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
-const mix = require('laravel-mix');
-let path = require('path');
+const mix = require("laravel-mix");
+let path = require("path");
 
 /*
  |--------------------------------------------------------------------------
@@ -14,8 +14,8 @@ let path = require('path');
 mix.webpackConfig({
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, 'resources/js/'),
-            ziggy: path.resolve('vendor/tightenco/ziggy/dist'),
+            "@": path.resolve(__dirname, "resources/js/"),
+            ziggy: path.resolve("vendor/tightenco/ziggy/dist")
         }
     }
 });
@@ -23,26 +23,29 @@ mix.webpackConfig({
 mix.disableSuccessNotifications();
 
 mix.options({
-        processCssUrls: false,
-        terser: {
-            extractComments: false,
-        }
-    })
-    .js('resources/js/app.js', 'public/js')
-    .js('resources/js/accounting.js', 'public/js')
-    .js('resources/js/fundraising.js', 'public/js')
-    .js('resources/js/visitors.js', 'public/js')
-    .js('resources/js/reports.js', 'public/js')
-    .js('resources/js/people.js', 'public/js')
-    .js('resources/js/bank.js', 'public/js')
-    .js('resources/js/library.js', 'public/js')
-    .js('resources/js/cmtyvol.js', 'public/js')
-    .js('resources/js/shop.js', 'public/js')
-    .js('resources/js/user_management.js', 'public/js')
-    .js('resources/js/editor.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts')
-    .copy('node_modules/summernote/dist/font', 'public/css/font')
+    processCssUrls: false,
+    terser: {
+        extractComments: false
+    }
+})
+    .js("resources/js/app.js", "public/js")
+    .js("resources/js/accounting.js", "public/js")
+    .js("resources/js/fundraising.js", "public/js")
+    .js("resources/js/visitors.js", "public/js")
+    .js("resources/js/reports.js", "public/js")
+    .js("resources/js/people.js", "public/js")
+    .js("resources/js/bank.js", "public/js")
+    .js("resources/js/library.js", "public/js")
+    .js("resources/js/cmtyvol.js", "public/js")
+    .js("resources/js/shop.js", "public/js")
+    .js("resources/js/user_management.js", "public/js")
+    .js("resources/js/editor.js", "public/js")
+    .sass("resources/sass/app.scss", "public/css")
+    .copy(
+        "node_modules/@fortawesome/fontawesome-free/webfonts",
+        "public/webfonts"
+    )
+    .copy("node_modules/summernote/dist/font", "public/css/font")
     .vue()
     .extract()
     .sourceMaps();
