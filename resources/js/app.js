28d83f1cf2c9259d4f70ebf53268d8667aa25c99
Nicolas Perrenoud <mail@nicu.ch>
2021-04-27 08:57:06 +0300

    separate files for app code

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,71 +2,11 @@ import "@/bootstrap";
 import $ from "jquery";
 import "@/vue";
 import "@/editor";
-
-/*====================================
-=            ON DOM READY            =
-====================================*/
-$(function() {
-    $(".toggle-nav").click(function() {
-        toggleNav();
-    });
-});
-
-/*========================================
-=            CUSTOM FUNCTIONS            =
-========================================*/
-
-function showNavigation() {
-    $(".site-wrapper").addClass("show-nav");
-    var overlay = $("#overlay_dark");
-    overlay.fadeIn("fast");
-    overlay.on("click", function() {
-        hideNavigation();
-    });
-}
-
-function hideNavigation() {
-    $(".site-wrapper").removeClass("show-nav");
-    var overlay = $("#overlay_dark");
-    overlay.fadeOut("fast");
-    overlay.off("click");
-}
-
-function toggleNav() {
-    if ($(".site-wrapper").hasClass("show-nav")) {
-        // Do things on Nav Close
-        hideNavigation();
-    } else {
-        // Do things on Nav Open
-        showNavigation();
-    }
-}
-
-$(function() {
-    // Delete confirmation method
-    $(".delete-confirmation").on("click", function() {
-        return confirm($(this).attr("data-confirmation"));
-    });
-
-    //  Context navigation
-    $(".context-nav-toggle").on("click", function() {
-        var nav = $(this).siblings(".context-nav");
-        var overlay = $("#overlay");
-        if (nav.is(":visible")) {
-            nav.fadeOut("fast");
-            overlay.fadeOut("fast");
-        } else {
-            nav.fadeIn("fast");
-            overlay.fadeIn("fast");
-            overlay.on("click", function() {
-                if ($(".context-nav").is(":visible")) {
-                    nav.fadeOut("fast");
-                    overlay.fadeOut("fast");
-                }
-            });
-        }
-    });
-});
+import "@/navigation";
+import "@/snackbar";
+import "@/tagify";
+import "@/shareUrl";
+import "fslightbox";
 
 // Elements with the selector class gain focus and have their cursor set to the end
 $(function() {
@@ -79,102 +19,11 @@ $(function() {
     });
 });
 
-//
-// Snackbar
-//
-import Snackbar from "node-snackbar";
-
-$(function() {
-    $(".snack-message").each(function() {
-        Snackbar.show({
-            text: $(this).html(),
-            duration: $(this).data("duration")
-                ? $(this).data("duration")
-                : 2500,
-            pos: "bottom-center",
-            actionText: $(this).data("action") ? $(this).data("action") : null,
-            actionTextColor: null,
-            customClass: $(this).data("class")
-        });
-    });
-});
-
-// Fullscreen lightbox
-import "fslightbox";
-
-/**
- * Tags input
- */
-import Tagify from "@yaireo/tagify";
-var tagifyAjaxController; // for aborting the call
-$(document).ready(function() {
-    document.querySelectorAll("input.tags").forEach(input => {
-        var suggestions =
-            input.getAttribute("data-suggestions") != null
-                ? JSON.parse(input.getAttribute("data-suggestions"))
-                : [];
-        var tagify = new Tagify(input, {
-            whitelist: suggestions
-        });
-
-        var suggestionsUrl = input.getAttribute("data-suggestions-url");
-        if (suggestionsUrl) {
-            tagify.on("input", function(e) {
-                var value = e.detail;
-                tagify.settings.whitelist.length = 0; // reset the whitelist
-
-                // https://developer.mozilla.org/en-US/docs/Web/API/AbortController/abort
-                tagifyAjaxController && tagifyAjaxController.abort();
-                tagifyAjaxController = new AbortController();
-
-                fetch(suggestionsUrl + value, {
-                    signal: tagifyAjaxController.signal
-                })
-                    .then(RES => RES.json())
-                    .then(function(whitelist) {
-                        tagify.settings.whitelist = whitelist;
-                        tagify.dropdown.show.call(tagify, value); // render the suggestions dropdown
-                    });
-            });
-        }
-    });
-});
-
 /**
  * Method for sending post request
  */
 import { postRequest } from "@/utils/form";
 window.postRequest = postRequest;
-
-//
-// Share an URL
-//
-$(function() {
-    $('[rel="share-url"]').on("click", function() {
-        var url = $(this).data("url");
-        if (navigator.share) {
-            navigator
-                .share({
-                    title: document.title,
-                    url: url
-                })
-                .then(() => console.log("Successful share"))
-                .catch(error => console.log("Error sharing", error));
-        } else {
-            var dummy = $("<input>")
-                .val(url)
-                .appendTo("body")
-                .select();
-            document.execCommand("copy");
-            dummy.remove();
-            Snackbar.show({
-                text: "Copied URL to clipboard.",
-                duration: 2500,
-                pos: "bottom-center"
-            });
-        }
-    });
-});
 
 /**
  * Input list with date range
---
+++ resources/js/navigation.js
@@ -0,0 +1,66 @@
+import $ from "jquery";
+
+/*====================================
+=            ON DOM READY            =
+====================================*/
+$(function() {
+    $(".toggle-nav").click(function() {
+        toggleNav();
+    });
+});
+
+/*========================================
+=            CUSTOM FUNCTIONS            =
+========================================*/
+
+function showNavigation() {
+    $(".site-wrapper").addClass("show-nav");
+    var overlay = $("#overlay_dark");
+    overlay.fadeIn("fast");
+    overlay.on("click", function() {
+        hideNavigation();
+    });
+}
+
+function hideNavigation() {
+    $(".site-wrapper").removeClass("show-nav");
+    var overlay = $("#overlay_dark");
+    overlay.fadeOut("fast");
+    overlay.off("click");
+}
+
+function toggleNav() {
+    if ($(".site-wrapper").hasClass("show-nav")) {
+        // Do things on Nav Close
+        hideNavigation();
+    } else {
+        // Do things on Nav Open
+        showNavigation();
+    }
+}
+
+$(function() {
+    // Delete confirmation method
+    $(".delete-confirmation").on("click", function() {
+        return confirm($(this).attr("data-confirmation"));
+    });
+
+    //  Context navigation
+    $(".context-nav-toggle").on("click", function() {
+        var nav = $(this).siblings(".context-nav");
+        var overlay = $("#overlay");
+        if (nav.is(":visible")) {
+            nav.fadeOut("fast");
+            overlay.fadeOut("fast");
+        } else {
+            nav.fadeIn("fast");
+            overlay.fadeIn("fast");
+            overlay.on("click", function() {
+                if ($(".context-nav").is(":visible")) {
+                    nav.fadeOut("fast");
+                    overlay.fadeOut("fast");
+                }
+            });
+        }
+    });
+});
