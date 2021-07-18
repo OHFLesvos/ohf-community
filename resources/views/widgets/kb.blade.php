31e8fb9e4311c3c5d3c825ba85a450b21fee3fe9
Nicolas Perrenoud <mail@nicu.ch>
2021-07-18 17:38:41 +0300

    Fix latest article widget if no latest article exists

--- resources/views/widgets/kb.blade.php
+++ resources/views/widgets/kb.blade.php
@@ -10,13 +10,13 @@
             __('Articles in the database') => $num_articles,
         ],
     ])
-    <div class="card-body p-3 border-top">
-        @isset($latest_article)
+    @isset($latest_article)
+        <div class="card-body p-3 border-top">
             @lang('The latest article is <a href=":href">:title</a>, added :date.', [
                 'title' => $latest_article->title,
                 'href' => route('kb.articles.show', $latest_article),
                 'date' => $latest_article->created_at->diffForHumans(),
             ])
-        @endisset
     </div>
+    @endisset
 @endsection
