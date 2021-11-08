94216c16700087cf9862a9f01875aa71e536f374
Nicolas Perrenoud <mail@nicu.ch>
2021-11-08 16:35:46 +0200

    Added scheduler command to anonymize data

---
+++ app/Console/Commands/AnonymizeVisitors.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Visitors\Visitor;
+use Illuminate\Console\Command;
+
+class AnonymizeVisitors extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'visitors:anonymize';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Anonymizes visitor records who have not checked in more than one month.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $visitors = Visitor::where('anonymized', false)
+            ->whereDoesntHave('checkins', function ($qry) {
+                $qry->whereDate('created_at', '>=', now()->subMonth()->toDateString());
+            })->get();
+
+        $visitors->each(function (Visitor $visitor) {
+            $visitor->name = sha1($visitor->name);
+            $visitor->id_number = sha1($visitor->id_number);
+            if ($visitor->date_of_birth !== null) {
+                $visitor->date_of_birth = $visitor->date_of_birth->startOfYear();
+            }
+            $visitor->anonymized = true;
+            $visitor->save();
+        });
+
+        $this->info("Anonymized {$visitors->count()} visitors.");
+
+        return Command::SUCCESS;
+    }
+}
