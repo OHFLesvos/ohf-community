2895a19f4d8e07e8bb98f153166cd24750ca44d9
Nicolas Perrenoud <nicolas@ohf-lesvos.org>
2021-11-11 10:07:52 +0200

    v3.4.4

--- config/app.php
+++ config/app.php
@@ -19,7 +19,7 @@ return [
 
     'product_url' => 'https://github.com/OHFLesvos/ohf-community',
 
-    'version' => 'v3.4.3',
+    'version' => 'v3.4.4',
     /*
     |--------------------------------------------------------------------------
     | Application Environment
