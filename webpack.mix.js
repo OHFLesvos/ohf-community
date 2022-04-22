35fc07bfefe681490c9dfd2633ffd6ecb0f5b250
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2022-04-22 12:57:15 +0300

    Added eslint webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,8 @@
 const mix = require('laravel-mix');
 let path = require("path");
 
+require('laravel-mix-eslint')
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -22,6 +24,18 @@ mix.webpackConfig({
         chunkFilename: "js/chunks/[name].js?id=[chunkhash]"
     }
 });
+
+mix.eslint({
+    fix: true,
+    extensions: ['js'],
+    files: 'resources/js',
+    exclude: [
+        'node_modules',
+        'vendor',
+        'resources/js/ziggy.js',
+        'resources/js/vue-i18n-locales.generated.js',
+    ]
+})
 
 mix.disableSuccessNotifications();
 
