0bd662d996a6ea8291f4e2abf5fb74687cbfdbbb
Nicolas Perrenoud <mail@nicu.ch>
2022-09-21 11:44:27 +0300

    test names

--- tests/Unit/NumberFormatUtilTest.php
+++ tests/Unit/NumberFormatUtilTest.php
@@ -2,38 +2,38 @@
 
 use App\Util\NumberFormatUtil;
 
-test('bytesToHuman 100 B', function () {
+test('100 is 100 B', function () {
     expect(NumberFormatUtil::bytesToHuman(100))->toBe('100 B');
 });
 
-test('bytesToHuman 999 B', function () {
+test('999 is 999 B', function () {
     expect(NumberFormatUtil::bytesToHuman(999))->toBe('999 B');
 });
 
-test('bytesToHuman 1000 B', function () {
+test('1000 is 1000 B', function () {
     expect(NumberFormatUtil::bytesToHuman(1000))->toBe('1000 B');
 });
 
-test('bytesToHuman 1 KB', function () {
+test('1024 is 1 KB', function () {
     expect(NumberFormatUtil::bytesToHuman(1024))->toBe('1 KB');
 });
 
-test('bytesToHuman 1.02 KB', function () {
+test('1048 is 1.02 KB', function () {
     expect(NumberFormatUtil::bytesToHuman(1048))->toBe('1.02 KB');
 });
 
-test('bytesToHuman 1 MB', function () {
+test('126976 is 1 MB', function () {
     expect(NumberFormatUtil::bytesToHuman(1024 * 1024))->toBe('1 MB');
 });
 
-test('bytesToHuman 2 MB', function () {
+test('253952 is 2 MB', function () {
     expect(NumberFormatUtil::bytesToHuman(2 * 1024 * 1024))->toBe('2 MB');
 });
 
-test('bytesToHuman 1 GB', function () {
+test('1073741824 is 1 GB', function () {
     expect(NumberFormatUtil::bytesToHuman(1024 * 1024 * 1024))->toBe('1 GB');
 });
 
-test('bytesToHuman 1.5 GB', function () {
+test('1610612736 1.5 GB', function () {
     expect(NumberFormatUtil::bytesToHuman(1.5 * 1024 * 1024 * 1024))->toBe('1.5 GB');
 });
