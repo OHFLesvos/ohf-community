0e6ced60b68ba78c5d43464a16402cfff60cd9f2
Nicolas Perrenoud <mail@nicu.ch>
2022-09-25 17:24:46 +0300

    Update copyright string

--- resources/views/layouts/include/copyright.blade.php
+++ resources/views/layouts/include/copyright.blade.php
@@ -1,2 +1,2 @@
-This is <a href="https://github.com/OHFLesvos/ohf-community" target="_blank">open-source software</a>
+This application is based on <a href="https://github.com/OHFLesvos/ohf-community" target="_blank">open-source software</a>
 developed by <a href="https://ohf.gr" target="_blank">One Happy Family</a>.
