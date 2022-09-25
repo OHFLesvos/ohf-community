51b87891278286aa2e626ceed7ba802c7171003f
Nicolas Perrenoud <mail@nicu.ch>
2022-09-25 21:05:35 +0300

    Use official vue2 plugin for vite

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import path from "path";
-const { createVuePlugin } = require("vite-plugin-vue2");
+import vue from '@vitejs/plugin-vue2'
 
 export default defineConfig({
     plugins: [
         laravel(["resources/js/app.js"]),
-        createVuePlugin()
+        vue(),
     ],
     resolve: {
         alias: {
             "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
-            // ziggy: path.resolve(__dirname, "vendor/tightenco/ziggy/dist"),
         },
     },
     server: {
