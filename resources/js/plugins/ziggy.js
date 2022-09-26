f5c0c88c89efa5705cdaf2aaf532e59e1fe8ce14
Nicolas Perrenoud <mail@nicu.ch>
2022-09-26 08:53:03 +0300

    Use composer provided ziggy package

--- resources/js/plugins/ziggy.js
+++ resources/js/plugins/ziggy.js
@@ -1,4 +1,4 @@
-import ziggyRoute from 'ziggy-js'
+import ziggyRoute from 'ziggy'
 import { Ziggy } from '@/ziggy'
 export default function (name, params) {
     return ziggyRoute(name, params, false, Ziggy)
--- vite.config.js
+++ vite.config.js
@@ -23,6 +23,7 @@ export default defineConfig({
     resolve: {
         alias: {
             "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+            "ziggy": path.resolve(__dirname, 'vendor/tightenco/ziggy/dist/index.m.js'),
         },
     },
     server: {
