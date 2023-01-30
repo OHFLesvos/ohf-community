f28fad23e8eb10c2b44a57311804058c3e5fe1b7
Nicolas Perrenoud <mail@nicu.ch>
2023-01-30 08:39:20 +0200

    Package updates

---
+++ update-deps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SAIL='vendor/bin/sail'
+
+$SAIL composer update && \
+    $SAIL npm update && \
+    $SAIL npm run build && \
+    $SAIL pint && \
+    $SAIL php vendor/bin/phpstan analyze
