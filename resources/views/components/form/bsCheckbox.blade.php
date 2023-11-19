9f8d47d893ba93101883fb52661eed09d4b176e5
Nicolas Perrenoud <mail@nicu.ch>
2023-11-19 12:38:07 +0100

    removed unused blade components

--- resources/views/components/form/bsCheckbox.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="custom-control custom-checkbox">
-        {{ Form::checkbox($name, $value, $checked, [ 'class' => 'custom-control-input'.($errors->has($name) ? ' is-invalid' : ''), 'id' => $id ?? form_id_string($name) ]) }}
-        <label class="custom-control-label" for="{{ $id ?? form_id_string($name) }}">{{ $label }}</label>
-        @if ($errors->has($name))
-                <span class="invalid-feedback">{{ $errors->first($name) }}</span>
-        @endif
-</div>
--- resources/views/components/links/whatsapp.blade.php
+++
@@ -1,8 +0,0 @@
-{{-- Usage:
-    <x-links.whatsapp>+1 234 56 78 90</x-links.whatsapp>
-    <x-links.whatsapp message="Hello world!">+1 234 56 78 90</x-links.whatsapp>
---}}
-@props([
-    'message' => null
-])
-{!! whatsapp_link($slot, $message) !!}
